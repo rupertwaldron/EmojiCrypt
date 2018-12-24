3b836e80b276e1d39950817b974118dbe27a064b
rupertwaldron <rupert.waldron@yahoo.co.uk>
2018-12-24 21:51:30 +0000

    Initial Commit

---
+++ EmojiCrypt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EmojiCrypt
+//
+//  Created by Rupert Waldron on 24/12/2018.
+//  Copyright © 2018 Rupert Waldron. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
